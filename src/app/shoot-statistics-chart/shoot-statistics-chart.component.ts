be3a296d885df4f00309a6a0379a3e5d60e4bcac
Faycal Hrid <faycalhrid3@gmail.com>
2019-05-30 13:38:34 +0000

    30/05

--- src/app/shoot-statistics-chart/shoot-statistics-chart.component.ts
+++ src/app/shoot-statistics-chart/shoot-statistics-chart.component.ts
@@ -8,6 +8,7 @@ import {HttpClient} from '@angular/common/http';
 })
 export class ShootStatisticsChartComponent implements OnInit {
 
+  private availableDates:any;
 
   statByDate: any;
 
@@ -25,28 +26,31 @@ export class ShootStatisticsChartComponent implements OnInit {
 
   lineChartType = 'line';
 
-  lineChartLabels:any;
+  lineChartLabels: any;
 
 
   constructor(private http: HttpClient) {
-    this.lineChartLabels =  this.getShootStatisticsDates();
-    // for (let i = 0; i < this.lineChartLabels.length; i++) {
-    //   this.getStatisticsByDate(this.ssDates[i]);
-    //   this.lineChartData[0].data.push(this.statByDate["shooted_volume"]);
-    console.log(this.lineChartLabels);
-    // }
-    // this.getStatisticsByDate(this.ssDates[0]);
+
   }
 
   ngOnInit() {
+    this.getShootStatisticsDates();
+    console.log(this.availableDates);
+    // this.getShootStatisticsDates();
+    // this.getStatisticsByDate(this.lineChartLabels[0]);
   }
 
   getShootStatisticsDates() {
-    return this.http.get('http://127.0.0.1:8000/api/shootstatisticsdates').subscribe(ssDates => {this.lineChartLabels = ssDates['success'];});
+    return this.http.get('http://127.0.0.1:8000/api/shootstatisticsdates').subscribe(ssDates => {
+    this.lineChartLabels = ssDates['success'];
+    this.availableDates = ssDates['success'];
+    });
   }
 
   getStatisticsByDate(date) {
-    return this.http.get('http://127.0.0.1:8000/api/shootstatisticsbydate/' + date).subscribe(ssDates => {this.statByDate = ssDates['success'];});
+    return this.http.get('http://127.0.0.1:8000/api/shootstatisticsbydate/' + date).subscribe(ssDates => {this.statByDate = ssDates['success'];
+    // console.log(ssDates['success']);
+    });
   }
 
 
